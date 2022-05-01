2217e4ec551b76e1c9eb114c980a7c99da56fc97
wsavoie <wsavoie@gatech.edu>
2022-04-30 21:38:41 -0400

    created working dockerfiles and docker compose

--- app/oauth2.py
+++ app/oauth2.py
@@ -1,4 +1,5 @@
 from datetime import datetime, timedelta
+from typing import Optional
 
 from fastapi import Depends, HTTPException, status
 from fastapi.security import OAuth2PasswordBearer
@@ -14,7 +15,7 @@ SECRET_KEY = settings.secret_key
 ALGORITHM  = settings.algorithm
 ACCESS_TOKEN_EXPIRE_MINUTES = settings.access_token_expire_minutes
 
-def create_access_token(data: dict, expires_delta : timedelta | None = None):
+def create_access_token(data: dict, expires_delta : Optional[timedelta]):
     to_encode = data.copy()
     
     if expires_delta:
